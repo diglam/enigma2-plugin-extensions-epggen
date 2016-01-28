29f6338a013200a7491929f96fb753ed3135ef03
diglam <diglam@users.noreply.github.com>
2016-01-28 22:14:26 +0200

    Update cp4pc.sh

--- src/EPGgen/cp4pc.sh
+++ src/EPGgen/cp4pc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "New EPGgen directory..."
-mkdir /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+echo "New EPGregen directory..."
+mkdir /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
 
 echo "Copying files for enigma2PC..."
-cp -R ./locale /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+cp -R ./locale /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
 
-cp -R ./plugin.png /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+cp -R ./plugin.png /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
 
-cp -R ./__init__.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+cp -R ./__init__.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
 
-cp -R ./plugin.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+cp -R ./plugin.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
 
-#cp -R ./ui.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+#cp -R ./ui.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
 
-#cp -R ./fn.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGgen
+#cp -R ./fn.py /usr/local/e2/lib/enigma2/python/Plugins/Extensions/EPGregen
