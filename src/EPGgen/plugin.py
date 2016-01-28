8a895ba2664eff12ccf18e1f380aa8ba4eb20618
diglam <diglam@gmail.com>
2016-01-28 22:10:59 +0200

    buildups

--- src/EPGgen/plugin.py
+++ src/EPGgen/plugin.py
@@ -1,8 +1,9 @@
 # for localized messages
-"""EPGgen->plugin."""
-
+"""EPGregen->plugin."""
+import pprint
 import enigma
-from enigma import eServiceCenter
+import urllib
+import Screens
 from Screens.Screen import Screen
 from Components.Label import Label
 from Components.ActionMap import ActionMap
@@ -21,11 +22,71 @@ except:
     pass
 
 
-class EPGgenConfig(ConfigListScreen, Screen):
+def filterName(name):
+    if name is not None:
+        name = name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+    return name
+
+
+def getServiceInfoString(info, what):
+    v = info.getInfo(what)
+    if v == -1:
+        return "N/A"
+    if v == -2:
+        return info.getInfoString(what)
+    return v
+
+
+def getCurrentService(session):
+    try:
+        info = session.nav.getCurrentService().info()
+        return {
+            "result": True,
+            "name": filterName(info.getName()),
+            "namespace": getServiceInfoString(info, enigma.iServiceInformation.sNamespace),
+            "aspect": getServiceInfoString(info, enigma.iServiceInformation.sAspect),
+            "provider": getServiceInfoString(info, enigma.iServiceInformation.sProvider),
+            "width": getServiceInfoString(info, enigma.iServiceInformation.sVideoWidth),
+            "height": getServiceInfoString(info, enigma.iServiceInformation.sVideoHeight),
+            "apid": getServiceInfoString(info, enigma.iServiceInformation.sAudioPID),
+            "vpid": getServiceInfoString(info, enigma.iServiceInformation.sVideoPID),
+            "pcrpid": getServiceInfoString(info, enigma.iServiceInformation.sPCRPID),
+            "pmtpid": getServiceInfoString(info, enigma.iServiceInformation.sPMTPID),
+            "txtpid": getServiceInfoString(info, enigma.iServiceInformation.sTXTPID),
+            "tsid": getServiceInfoString(info, enigma.iServiceInformation.sTSID),
+            "onid": getServiceInfoString(info, enigma.iServiceInformation.sONID),
+            "sid": getServiceInfoString(info, enigma.iServiceInformation.sSID),
+            "ref": urllib.quote(getServiceInfoString(info, enigma.iServiceInformation.sServiceref), safe=' ~@#$&()*!+=:;,.?/\''),
+            "iswidescreen": info.getInfo(enigma.iServiceInformation.sAspect) in (3, 4, 7, 8, 0xB, 0xC, 0xF, 0x10)
+        }
+    except Exception, e:
+        # pprint.pprint(e)
+        return {
+            "result": False,
+            "name": "",
+            "namespace": "",
+            "aspect": 0,
+            "provider": "",
+            "width": 0,
+            "height": 0,
+            "apid": 0,
+            "vpid": 0,
+            "pcrpid": 0,
+            "pmtpid": 0,
+            "txtpid": "N/A",
+            "tsid": 0,
+            "onid": 0,
+            "sid": 0,
+            "ref": "",
+            "iswidescreen": False
+        }
+
+
+class EPGregenConfig(ConfigListScreen, Screen):
     if HD:
         skin = """
         <screen position="center,center" size="600,500" \
-            title="EPGgen Configuration (HD skin)" >
+            title="EPGregen Configuration (HD skin)" >
         <ePixmap name="red"    position="0,0"   zPosition="2" size="140,40" \
             pixmap="skin_default/buttons/red.png" transparent="1" \
             alphatest="on" />
@@ -75,7 +136,7 @@ class EPGgenConfig(ConfigListScreen, Screen):
     else:
         skin = """
         <screen position="center,center" size="600,430" \
-            title="EPGgen Configuration (SD skin)" >
+            title="EPGregen Configuration (SD skin)" >
         <ePixmap name="red" position="0,0" zPosition="2" size="140,40" \
             pixmap="skin_default/buttons/red.png" transparent="1" \
             alphatest="on" />
@@ -125,8 +186,8 @@ class EPGgenConfig(ConfigListScreen, Screen):
 
     def __init__(self, session, args=None):
         self.session = session
-        self.skin = EPGgenConfig.skin
-        self.setup_title = _("EPGgen Configuration")
+        self.skin = EPGregenConfig.skin
+        self.setup_title = _("EPGregen Configuration")
         Screen.__init__(self, session)
         self["status"] = Label()
         self["statusbar"] = Label()
@@ -136,27 +197,20 @@ class EPGgenConfig(ConfigListScreen, Screen):
         self["key_blue"] = Button(_("Sources"))
         self["myActionMap"] = ActionMap(["SetupActions", "ColorActions"],
                                         {
-            "yellow": self.doManual,
+            "yellow": self.openManual,
             "cancel": self.close  # RC Command "cancel" to close your Screen
         }, -1)
 
-    def doManual(self, **kwargs):
-        print "\n[EPGgen] -> doManual\n"
-        self.session.open(EPGgenManual, kwargs["servicelist"])
-
-        bouquet = self.servicelist.bouquet_root
-        serviceHandler = eServiceCenter.getInstance()
-        bouquetlist = serviceHandler.list(bouquet)
-        bouquet = bouquetlist.getNext()
-        servicelist = serviceHandler.list(bouquet)
-        print servicelist
+    def openManual(self, **kwargs):
+        print "\n[EPGregen] -> openManual\n"
+        self.session.open(EPGregenManual)
 
 
-class EPGgenManual(Screen):
+class EPGregenManual(Screen):
     "gasdgasgdasgadg Manual"
     skin = """
-    <screen name="EPGgenManual" position="center,center" size="600,430" \
-            title="EPGgen Manual" >
+    <screen name="EPGregenManual" position="center,center" size="600,430" \
+            title="EPGregen Manual" >
     <ePixmap name="red" position="0,0" zPosition="2" size="140,40" \
             pixmap="skin_default/buttons/red.png" transparent="1" \
             alphatest="on" />
@@ -199,28 +253,46 @@ class EPGgenManual(Screen):
     def __init__(self, session, args=None):
         self.session = session
         Screen.__init__(self, session)
-        self["status"] = Label()
-        self["statusbar"] = Label()
         self["key_red"] = Button(_("Cancel"))
         self["key_green"] = Button(_("Save"))
-        self["key_yellow"] = Button(_("Manual"))
+        self["key_yellow"] = Button(_("Start"))
         self["key_blue"] = Button(_("Sources"))
-        self["myActionMap"] = ActionMap(["SetupActions"],
+        self["myActionMap"] = ActionMap(["SetupActions", "ColorActions"],
                                         {
+            "yellow": self.doManual,
             "cancel": self.close  # RC Command "cancel" to close your Screen
-        }, -2)
-        self.setTitle(_("EPGgen Manual"))
+        }, -1)
+        self.setTitle(_("EPGregen Manual"))
 
+    def doManual(self, **kwargs):
+        print "\n[EPGregen] -> doManual\n"
+        # fav = enigma.eServiceReference('1:0:19:2968:7919:42E:1A40000:0:0:0:(type == 1) || (type == 17) || (type == 195) || (type == 25) FROM BOUQUET "bouquets.tv" ORDER BY bouquet')
+        # fav = enigma.eServiceReference('1:7:1:0:0:0:0:0:0:0:(type == 1) || (type == 17) || (type == 195) || (type == 25) FROM BOUQUET "bouquets.tv" ORDER BY bouquet')
+        service_info = getCurrentService(global_session)
+        if service_info["result"] is True:
+            service_info_ref = service_info["ref"]
+            print("\nCurrent service: "+service_info["name"]+" - "+service_info["ref"]+"\n")
+        # pprint.pprint(service_info)
+        serviceHandler = enigma.eServiceCenter.getInstance()
+        services = serviceHandler.list(enigma.eServiceReference(
+            '%s ORDER BY name' % (Screens.ChannelSelection.service_types_tv)))
+        channels = services and services.getContent("SN", True)
+        print "\n[EPGregen] -> doManual -> channel list loaded\n"
+        # pprint.pprint(channels)
+        Servicelist = Screens.InfoBar.InfoBar.instance.servicelist
+        # Servicelist.startServiceRef = self.session.nav.getCurrentlyPlayingServiceOrGroup()
+        pprint.pprint(Servicelist.startServiceRef)
 
 def main(session, **kwargs):
-    print "\n<<<<<<< EPGgen started >>>>>>>\n"
-
-    session.open(EPGgenConfig)
+    print "\n<<<<<<< EPGregen started >>>>>>>\n"
+    global global_session
+    global_session = session
+    global_session.open(EPGregenConfig)
 
 
 def Plugins(**kwargs):
     return PluginDescriptor(
-        name="EPGgen",
+        name="EPGregen",
         description="EPG generator from satellite services",
         where=PluginDescriptor.WHERE_PLUGINMENU,
         icon="plugin.png",
