06882e596a1eb35b0b445db775fbca0d64727bd3
diglam <diglam@users.noreply.github.com>
2016-01-28 22:24:30 +0200

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup, Extension
 import setup_translate
 
 
-dreamcrc = Extension('Extensions/EPGgen/dreamcrc',
+dreamcrc = Extension('Extensions/EPGregen/dreamcrc',
                      sources=['dreamcrc.c'])
 
-pkg = 'Extensions.EPGgen'
-setup(name='enigma2-plugin-extensions-epggen',
+pkg = 'Extensions.EPGregen'
+setup(name='enigma2-plugin-extensions-epgregen',
       version='0.0.1',
       description='C implementation of Dream CRC32 algorithm',
       package_dir={pkg: 'EPGepg'},
